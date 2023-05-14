105346bc6326485d0530d989c4b501afc9ec02f2
Anshul Gupta <anshulgolu123@gmail.com>
2023-05-14 13:51:19 +0530

    Update CountWaystoGoodString.java

--- CountWaystoGoodString.java
+++ CountWaystoGoodString.java
@@ -18,7 +18,7 @@ static int count = 0;
         countHelper(count0, hi, lo, zeros, ones);
         countHelper(count1, hi, lo, zeros, ones);
     }
-
+    //Solution 1:Using recursion
     static public int countGoodStrings(int low, int high, int zero, int one) {
         String zeros = "0".repeat(zero);
         String ones = "1".repeat(one);
@@ -26,5 +26,18 @@ static int count = 0;
         countHelper("", high, low, zeros, ones);
         return count;
     }
+    //Solution 2:Using DP
+    static public int countGoodStrings(int lo, int high, int zero, int one) {
+        int[] count = new int[high+1];
+        int number = 0;
+        count[0] = 1;
+        for(int i=1;i<=high;i++){
+            count[i] = ((i>=zero ? count[i-zero]:0)+(i>=one ? count[i-one]:0))%1000000007;
+            if(i>=lo)
+                number= (number + count[i]) % 1000000007;
 
+        }
+        
+        return number;
+    }
 }
